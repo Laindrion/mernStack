2ff8fd57d21bbb0d3231464e11e304a1b41f325f
Owtp <igencap@gmail.com>
2024-09-15 18:13:17 +0300

    did

--- backend/server.js
+++ backend/server.js
@@ -30,6 +30,19 @@ app.post("/api/products", async (req, res) => {
 
 /* console.log(process.env.MONGO_URI); */ // see if the api is accesible
 
+
+app.delete("/api/products/:id", async (req, res) => {
+    const { id } = req.params;
+    console.log("id:", id);
+
+    try {
+        await Product.findByIdAndDelete(id);
+        res.status(200).json({ success: true, message: "Product deleted" });
+    } catch (error) {
+        
+    }
+})
+
 app.listen(5000, () => {
     connectDB();
     console.log("Server started at http://localhost:5000 , nothing changes if nothing changes");
