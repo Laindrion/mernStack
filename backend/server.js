69b7f3bac6ff3e105508e52c1160ccf5d5c43070
Lain <igencap@gmail.com>
2024-09-16 14:02:12 +0500

    started with front

--- backend/server.js
+++ backend/server.js
@@ -1,49 +1,19 @@
-import mongoose from "mongoose";
 import express from "express";
 import dotenv from "dotenv";
-import Product from "./models/product.model.js";
 import { connectDB } from "./config/db.js";
+
+import productRoutes from "./routes/product.route.js";
 
 dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 
 app.use(express.json()); // allows us to accept json data in a req.body
 
-app.post("/api/products", async (req, res) => {
-    const product = req.body; // user will send this data
+app.use("/api/products", productRoutes);
 
-    if (!product.name || !product.price || !product.image) {
-        return res.status(400).json({ success: false, message: "Please provide all fields" })
-    }
-
-    const newProduct = new Product(product);
-
-    try {
-        await newProduct.save();
-        res.status(201).json({ success: true, data: newProduct })
-    } catch (error) {
-        console.error("Error in Create product:", error.message);
-        res.status(500).json({ success: false, message: "Server error" });
-    }
-});
-
-/* console.log(process.env.MONGO_URI); */ // see if the api is accesible
-
-
-app.delete("/api/products/:id", async (req, res) => {
-    const { id } = req.params;
-    console.log("id:", id);
-
-    try {
-        await Product.findByIdAndDelete(id);
-        res.status(200).json({ success: true, message: "Product deleted" });
-    } catch (error) {
-        
-    }
-})
-
-app.listen(5000, () => {
+app.listen(PORT, () => {
     connectDB();
-    console.log("Server started at http://localhost:5000 , nothing changes if nothing changes");
+    console.log("Server started at http://localhost:" + PORT + ", " + "nothing changes if nothing changes");
 });
