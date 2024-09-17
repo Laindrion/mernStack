37ad21d87ce987541aa65ce74d01304584333e2e
Lain <igencap@gmail.com>
2024-09-17 17:04:31 +0500

    did

--- frontend/src/pages/CreatePage.jsx
+++ frontend/src/pages/CreatePage.jsx
@@ -1,6 +1,44 @@
+import { Box, Container, VStack, Heading, useColorModeValue, Input } from "@chakra-ui/react";
+import { useState } from "react";
+
 const CreatePage = () => {
+  const [newProduct, setNewProduct] = useState({
+    name: "",
+    price: "",
+    image: "",
+  });
   return (
-    <div>CreatePage</div>
+    <Container maxW={"container.sms"}>
+      <VStack
+        spacing={8}>
+        <Heading as={"h1"} size={"2x1"} textAlign={"center"}>
+          Create New Prdouct
+        </Heading>
+
+        <Box
+          w={"full"} bg={useColorModeValue("white", "gray.800")}
+          p={6} rounded={"lg"} shadow={"md"}
+        >
+          <VStack spacing={4}>
+            <Input
+              placeholde="Product Name"
+              name="name"
+              value={newProduct.name} onChange={(e) => setNewProduct({ ...newProduct, name: e.target.value })} />
+
+            <Input
+              placeholde="Price"
+              name="price"
+              type="number"
+              value={newProduct.price} onChange={(e) => setNewProduct({ ...newProduct, price: e.target.value })} />
+
+            <Input
+              placeholde="Image URL"
+              name="image"
+              value={newProduct.image} onChange={(e) => setNewProduct({ ...newProduct, image: e.target.value })} />
+          </VStack>
+        </Box>
+      </VStack>
+    </Container >
   )
 }
 
